8b10c099bcebd50dc31e105ed98fcf175f07f428
GeonHee <relu00123@gmail.com>
2024-06-03 19:32:55 +0900

    Section2. Listener

---
+++ Server/DummyClient/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DNS (Domain Name System ) 
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+
+            // 첫번째 인자는 ip : 식당 이름
+            // 두번째 인자는 포트번호 : 식당 정문인지 후문인지
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            while (true)
+            {
+                // 휴대폰 설정
+                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    // 문지기한테 입장 문의
+                    // 빠져 나오면 연결한 것임
+                    socket.Connect(endPoint);
+
+                    // RemoteEndPoint -> 연결된 반대쪽 IP
+                    Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+
+                    // 보낸다.
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                    int sendBytes = socket.Send(sendBuff);
+
+                    // 받는다.
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = socket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Server] {recvData}");
+
+                    // 나간다.
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                Thread.Sleep(100);
+
+            }
+        }
+    }
+}
---
+++ Server/ServerCore/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static Listener _listener = new Listener();
+
+        static void OnAcceptHandler(Socket clientSocket)
+        {
+            try
+            {
+                // 받는다
+                // 손님이 보내준 데이터는 recvBuff에 저장된다.
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = clientSocket.Receive(recvBuff); // Blocking 함수
+
+                // recvBuffer에 받은 값을 String으로 변환해준다. 
+                // 두번째 인자는 데이터 받을 시작 인덱스
+                // 세번째 인자는 몇 바이트를 받을 것인지
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Clinet] {recvData}");
+
+                // 보낸다
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server !");
+                clientSocket.Send(sendBuff); // Blocking 함수
+
+                // 쫓아낸다. 
+                clientSocket.Shutdown(SocketShutdown.Both); // 듣기도 싫고 말하기도 싫다.
+                clientSocket.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+             
+        }
+
+       static void Main(string[] args)
+        {
+            // DNS (Domain Name System ) 
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+
+            // 첫번째 인자는 ip : 식당 이름
+            // 두번째 인자는 포트번호 : 식당 정문인지 후문인지
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            _listener.init(endPoint, OnAcceptHandler);
+
+            while (true)
+            {
+                    
+            }
+      
+        }
+    }
+}
+
+
