2b8a27569bfbbd6583153618eb7c89b73b47dbd2
relu00123 <relu00123@gmail.com>
2024-06-05 02:44:44 +0900

    Section2. Connector

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using ServerCore;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using ServerCore;
 
 namespace DummyClient
 {
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected bytes: {endPoint}");
+
+            // 보낸다
+            for (int i = 0; i < 5; i++)
+            {
+                byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World! {i}");
+                Send(sendBuff);
+            }
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected bytes: {endPoint}");
+        }
+
+        public override void OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Server] {recvData}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -18,41 +51,16 @@ namespace DummyClient
             // 두번째 인자는 포트번호 : 식당 정문인지 후문인지
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
+
+            Connector connector = new Connector();
+            connector.Connect(endPoint, () => { return new GameSession(); });
+
+
             while (true)
             {
-                // 휴대폰 설정
-                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
                 try
                 {
-                    // 문지기한테 입장 문의
-                    // 빠져 나오면 연결한 것임
-                    socket.Connect(endPoint);
-
-                 
-
-                    // RemoteEndPoint -> 연결된 반대쪽 IP
-                    Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
-
-                    // 보낸다
-                    for (int i = 0; i < 5; i++)
-                    {
-                        byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World! {i}");
-                        int sendBytes = socket.Send(sendBuff);
-                    }
-
-          
-                    // 받는다.
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = socket.Receive(recvBuff);
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    Console.WriteLine($"[From Server] {recvData}");
-
-                    // 나간다.
-                    socket.Shutdown(SocketShutdown.Both);
-                    socket.Close();
                 }
-
 
                 catch (Exception e)
                 {
