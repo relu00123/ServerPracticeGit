30a431bc9846df3947862d693311cfb48eb76a61
relu00123 <relu00123@gmail.com>
2024-06-09 23:11:34 +0900

    Section4. 채팅 테스트 #2

--- Server/DummyClient/Packet/PacketHandler.cs
+++ Server/DummyClient/Packet/PacketHandler.cs
@@ -1,4 +1,5 @@
-﻿using ServerCore;
+﻿using DummyClient;
+using ServerCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +12,13 @@ class PacketHandler
 
    
 
-    public static void S_TestHandler(PacketSession session, IPacket packet)
+    public static void S_ChatHandler(PacketSession session, IPacket packet)
     {
+        S_Chat chatPacket = packet as S_Chat;
+        ServerSession serverSession = session as ServerSession;
 
+        //if (chatPacket.playerId == 1)
+            Console.WriteLine(chatPacket.chat);
     }
 
 }
--- Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+045f8f010411b2e39510497736cd582d7ff24509")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32cd256737b81812765e3169b9b7de0e08b1639d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
