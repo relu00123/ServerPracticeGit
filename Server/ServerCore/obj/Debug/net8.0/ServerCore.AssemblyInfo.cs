e2ef5aa6e31c99993e3ac5beff0c7b4726db5bd7
relu00123 <relu00123@gmail.com>
2024-06-10 12:31:17 +0900

    Section4. JobTimer

--- Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
+++ Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac79af6064ce4e7c77b4984c99572a9d09697475")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8803073740c50daf0e7ebd776e9b3fead6774691")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
